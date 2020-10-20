cff1a3368f7d3a077ca318ab5e7d030763ce5ac8
VIGANESHA H R <42531234+viganeshahr@users.noreply.github.com>
2020-10-20 11:17:56 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -3,9 +3,15 @@ class Calc{
   public static int add(int a,int b){
     return a+b;
   }
+  
+  public static int prod(int a,int b){
+    return a*b;
+  }
+  
   public static void main(String args[]){
       System.out.println(add(10,20));  
     System.out.println("THIS IS AN UPDATE");
+    System.out.println(prod(10,20));
   }
   
 }
