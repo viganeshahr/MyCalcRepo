3e233820bd7f181124976ceaf38e687ca95728b3
VIGANESHA H R <42531234+viganeshahr@users.noreply.github.com>
2020-10-20 10:55:43 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -4,7 +4,8 @@ class Calc{
     return a+b;
   }
   public static void main(String args[]){
-      System.out.println(add(10,20));    
+      System.out.println(add(10,20));  
+    System.out.println("THIS IS AN UPDATE");
   }
   
 }
